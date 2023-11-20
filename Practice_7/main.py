f2580308b0a4e04ff39185e8c3a0507d92718768
ilyak0n <ilya.konushkin@bk.ru>
2023-11-21 02:44:23 +0300

    practice 7

---
+++ Practice_7/main.py
@@ -0,0 +1,33 @@
+import re
+
+def get_books(stroke):
+    pattern = r"(?P<isbn>[\d-]+)(?:\|)(?P<title>[\w\s]+)(?:\|)(?P<author>[\w\s]+)(?:\|)(?P<quantity>\d+)(?:\|)(?P<price>[\d.\d]+)"
+    file = open("books.csv").read()
+    matches = re.findall(pattern, file)
+    arr = list()
+    for match in matches:
+        for i in range(len(match)):
+            if stroke in match[i]:
+                arr.append(list(match))
+    return arr
+
+def get_totals():
+    arr = get_books(gets)
+    new_arr = []
+    for i in range(len(arr)):
+        listik = (arr[i][0], float(arr[i][-1]) * int(arr[i][-2]))
+        new_arr.append(listik)
+
+    for i in range(len(new_arr)):
+        new_arr[i] = list(new_arr[i])
+        if new_arr[i][-1] < 500:
+            new_arr[i][-1] += 100
+
+    for i in range(len(new_arr)):
+        new_arr[i] = tuple(new_arr[i])
+
+    return new_arr
+
+gets = input()
+print(get_books(gets))
+print(get_totals())
